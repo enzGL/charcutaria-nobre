2c5b04ca436fc763e4fd5452cf5263155ddfa9fa
Enzo Lanzi <enzolanzi189@gmail.com>
2025-06-25 21:18:01 -0300

    mudança no texto do rodape

--- rodape.php
+++ rodape.php
@@ -1,8 +1,7 @@
 <footer class="rodape">
      <hr>
      <img src="img/porcu.webp">
-     <small class="texto-rodape"><?php echo ("Charcutaria Nobre"); ?></small>
-     <small class="texto-rodape"><?php echo date("Y"); ?></small>
+     <small class="texto-rodape"><?php echo date("© Y"); echo(" - Charcutaria Nobre") ?></small>
  </footer>
 
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>

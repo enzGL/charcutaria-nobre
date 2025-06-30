6fbe74c2bf0604d12575517b6c14446399419b81
Enzo Lanzi <enzolanzi189@gmail.com>
2025-06-30 19:43:33 -0300

    add favicon

--- cabecalho.php
+++ cabecalho.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/porcu.webp" type="image/x-icon">
     <title>Charcutaria Nobre</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
